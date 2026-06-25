2349d25da8c7c1bd3dc1a58986ca3604c72115e0
Tejvir.Sohi <Tejvir.Sohi@totalmobile.co.uk>
2026-06-25 16:38:08 +0100

    Add unit tests for read/watch time

--- app/components/card/Card.spec.tsx
+++ app/components/card/Card.spec.tsx
@@ -6,6 +6,7 @@ const mockCard = {
   title: "Test Card Title",
   thumbnail: "https://example.com/image.jpg",
   tags: ["Health", "Wellbeing", "Fitness"],
+  duration: 10
 };
 
 describe("When rendering the card component", () => {
@@ -29,8 +30,13 @@ describe("When rendering the card component", () => {
 
   it("should render no more than 3 tags", () => {
     const manyTags = ["Health", "Wellbeing", "Fitness", "Mental Health", "Support"];
-    render(<Card title={mockCard.title} thumbnail={mockCard.thumbnail} tags={manyTags} />);
+    render(<Card title={mockCard.title} thumbnail={mockCard.thumbnail} tags={manyTags} duration={mockCard.duration} />);
     const tagElements = document.querySelectorAll(".tag");
     expect(tagElements.length).toBeLessThanOrEqual(3);
+  });
+
+  it("should render a read/watch time in minutes", () => {
+    render(<Card {...mockCard} duration={mockCard.duration} />);
+    expect(screen.getByText(`${mockCard.duration} min read/watch time`)).toBeTruthy();
   });
 });
