bcc4c194ccf6a571ecba49940affa389c5c890a0
Tejvir.Sohi <Tejvir.Sohi@totalmobile.co.uk>
2026-06-25 18:00:48 +0100

    Fix UI issues which came from migrating layout responsibilties to category component

--- app/components/cards/Cards.component.tsx
+++ app/components/cards/Cards.component.tsx
@@ -1,21 +1,15 @@
+import type { Resource } from "../category/Category.component";
 import { Card } from "../card/Card.component";
-import "./cards.css";
-
-type CardItem = {
-  title: string;
-  thumbnail: string;
-  tags: string[];
-};
 
 type CardsProps = {
-  cards: CardItem[];
+  cards: Resource[];
 };
 
 export function Cards({ cards }: CardsProps) {
   return (
     <div className="cards-container">
       {cards.map((card) => (
-        <Card key={card.title} {...card} />
+        <Card key={card.id} {...card} />
       ))}
     </div>
   );
